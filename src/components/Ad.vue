242d9715ca9a1e74d69c417e7804396d9281dee7
Артем <user@WIN-QERL5BT18CI>
2024-12-18 19:19:45 +0500

    video page ads for mobile

---
+++ src/components/Ad.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="h-[180px] w-full bg-foreground-dark hidden items-center justify-center text-[20px]
+    inf-sm:max-md:flex">
+        <p>РЕКЛАМА</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/VideoPage.vue
+++ src/components/VideoPage.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="w-full h-max grid grid-cols-3 rounded-[15px] gap-[24px] bg-background
+    <div class="w-full h-max grid grid-cols-3 rounded-[15px] gap-[24px] mb-[10px] bg-background
     min-[940px]:max-[1250px]:grid-cols-2
     inf-sm:max-[940px]:grid-cols-1">
         <Video></Video>
         <Video></Video>
+        <Ad></Ad>
         <Video></Video>
         <Video></Video>
+        <Ad></Ad>
         <Video></Video>
         <Video></Video>
     </div>
@@ -13,6 +15,7 @@
 
 <script setup>
     import Video from './Video.vue';
+    import Ad from './Ad.vue'
 </script>
 
 <style lang="scss" scoped>
